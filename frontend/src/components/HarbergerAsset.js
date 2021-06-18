9847cd333d2e2e34adc76289cf728243c72705cf
Sahil Mehta <samehta91@gmail.com>
2021-06-18 15:35:51 -0700

    Update conditionals for collect and reclaim actions

--- frontend/src/components/HarbergerAsset.js
+++ frontend/src/components/HarbergerAsset.js
@@ -229,7 +229,7 @@ class HarbergerAsset extends Component {
           <Row>
             <Col className="d-flex justify-content-center">
               <div className="text-center my-3">
-                {!this.state.isLoadingToken && ((this.props.selectedAddress === this.props.creatorAddress && this.props.creatorBalance > 0) || (this.props.selectedAddress === this.props.adminAddress && this.props.adminBalance > 0)) ? (
+                {!this.props.isLoadingToken && (this.props.selectedAddress === this.props.creatorAddress || this.props.selectedAddress === this.props.adminAddress) ? (
                   <Button className="py-2 px-3" variant="success" onClick={(e) => {this.props.collectFunds()}}>Collect Funds</Button>
                 ) : (
                   null
@@ -240,7 +240,7 @@ class HarbergerAsset extends Component {
           <Row>
             <Col className="d-flex justify-content-center">
               <div className="text-center my-3">
-                {!this.state.isLoadingToken && this.props.selectedAddress === this.props.creatorAddress && this.props.timeExpired ? (
+                {!this.props.isLoadingToken && this.props.selectedAddress === this.props.creatorAddress ? (
                   <Button className="mb-5 py-2 px-3" variant="danger" onClick={(e) => {this.props.reclaimAsset()}}>Reclaim Asset</Button>
                 ) : (
                   null
