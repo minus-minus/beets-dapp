37ca8324d475613816a8204f4ecba211ddb89bc0
Sahil Mehta <samehta91@gmail.com>
2021-07-23 09:28:27 -0700

    Update collect event log

--- frontend/src/utils/HTAX/constants.js
+++ frontend/src/utils/HTAX/constants.js
@@ -8,7 +8,6 @@ export const HTAX_EVENT_ABI = [
   "event Deposit(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, address to, uint256 value)",
   "event Sale(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, address to, uint256 value)",
   "event Refund(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, address to, uint256 value)",
-  "event Collect(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, uint256 value)",
-  // "event Collect(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, address to, uint256 value)",
+  "event Collect(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, address to, uint256 value)",
   "event Foreclosure(uint256 indexed timestamp, uint256 indexed tokenId, address indexed from, address to)"
 ]
